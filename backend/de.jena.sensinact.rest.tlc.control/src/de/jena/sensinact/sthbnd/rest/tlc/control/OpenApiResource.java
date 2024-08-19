17aec1b4d8db4488eefe224499842e5588ad794c
Guido Grune <g.grune@datainmotion.com>
2024-08-19 21:19:16 +0200

    regenerate the license infos
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- backend/de.jena.sensinact.rest.tlc.control/src/de/jena/sensinact/sthbnd/rest/tlc/control/OpenApiResource.java
+++ backend/de.jena.sensinact.rest.tlc.control/src/de/jena/sensinact/sthbnd/rest/tlc/control/OpenApiResource.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012 - 2024 Data In Motion and others.
+ * All rights reserved. 
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ */
+
 //package de.jena.sensinact.sthbnd.rest.tlc.control;
 //
 //import java.util.Map;
--- backend/de.jena.ttn.sensinact.model/src/de/jena/model/sensinact/ttnprovider/TtnTemperatureSensor.java
+++ backend/de.jena.ttn.sensinact.model/src/de/jena/model/sensinact/ttnprovider/TtnTemperatureSensor.java
@@ -1,15 +1,16 @@
 /*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation.
- * 
+ * Copyright (c) 2012 - 2024 Data In Motion and others.
+ * All rights reserved. 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
- *   Data In Motion - initial API and implementation 
+ *     Data In Motion - initial API and implementation
  */
+
 package de.jena.model.sensinact.ttnprovider;
 
 import org.osgi.annotation.versioning.ProviderType;
