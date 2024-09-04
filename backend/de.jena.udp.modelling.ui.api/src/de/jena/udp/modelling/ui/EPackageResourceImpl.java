3e6cf08baa2bf3b92aa257b00ea0088d80874792
Guido Grune <g.grune@datainmotion.com>
2024-09-04 13:42:12 +0200

    Fix applicationID
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- backend/de.jena.udp.modelling.ui.api/src/de/jena/udp/modelling/ui/EPackageResourceImpl.java
+++ backend/de.jena.udp.modelling.ui.api/src/de/jena/udp/modelling/ui/EPackageResourceImpl.java
@@ -42,7 +42,7 @@ import jakarta.ws.rs.core.Response;
 @RequireEMFJson
 @JakartarsResource
 @Component(name = EPackageResourceImpl.COMPONENT_NAME, service = EPackageResource.class, scope = ServiceScope.PROTOTYPE)
-@JakartarsApplicationSelect("(id=updui)")
+@JakartarsApplicationSelect("(applicationId=updui)")
 @Path("/epackage")
 public class EPackageResourceImpl implements EPackageResource {
 	private static final Logger LOGGER = System.getLogger(EPackageResourceImpl.class.getName());
