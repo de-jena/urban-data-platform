dbe1e7b8584d25f1daf8adabb4f0bbe078effece
Guido Grune <g.grune@datainmotion.com>
2025-06-03 15:35:29 +0200

    add simple test case
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- backend/de.jena.glt.rest/test/de/jena/glt/rest/GltOpenApiTest.java
+++ backend/de.jena.glt.rest/test/de/jena/glt/rest/GltOpenApiTest.java
@@ -1,14 +1,25 @@
 package de.jena.glt.rest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import de.jena.glt.rest.GltOpenApi.OpenApiConfig;
 
 class GltOpenApiTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		GltOpenApi gltOpenApi = new GltOpenApi();
+		OpenApiConfig conf = Mockito.mock(OpenApiConfig.class);
+		when(conf.apiKey()).thenReturn("1234");
+		gltOpenApi.activate(conf);
+		Map<String,Object> header = gltOpenApi.getHeader();
+		assertThat(header).containsEntry("x-api-key", "1234");
 	}
 
 }
