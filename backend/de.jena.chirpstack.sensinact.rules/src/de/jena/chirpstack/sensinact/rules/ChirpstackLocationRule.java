09c5da23f3c964a020cae73537410d2f19307ea5
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-11-24 15:04:25 +0100

    license
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- backend/de.jena.chirpstack.sensinact.rules/src/de/jena/chirpstack/sensinact/rules/ChirpstackLocationRule.java
+++ backend/de.jena.chirpstack.sensinact.rules/src/de/jena/chirpstack/sensinact/rules/ChirpstackLocationRule.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
 package de.jena.chirpstack.sensinact.rules;
 
 import java.time.Instant;
@@ -26,26 +39,29 @@ public class ChirpstackLocationRule implements RuleDefinition{
 	@Override
 	public void evaluate(List<ProviderSnapshot> providerSnapshots, ResourceUpdater resourceUpdater) {
 
-//		ProviderSnapshot provider = findRuleTriggerProvider(providerSnapshots);
-		for(ProviderSnapshot provider : providerSnapshots) {
-			LOGGER.info(String.format("Provider that triggered the rule is %s", provider.getName()));
-			//We filter out all the chirpstack-xxxx which are not of type Teros21
-			if(provider.getService("sensor") == null) return;
-			if(provider.getService("sensor").getResource("potentialWater1") == null) return;
-
-			String providerId = provider.getName();
-			GeoJsonObject location = (GeoJsonObject) provider.getService("admin").getResource("location").getValue().getValue();
-			if(location != null) {
-				resourceUpdater.updateResource(provider.getName().concat("-derived"), "admin", "location", location, Instant.now());
-				LOGGER.info(String.format("Updated location for provider %s", providerId));
-			}			
-		}
+		ProviderSnapshot provider = findRuleTriggerProvider(providerSnapshots);
+//		for(ProviderSnapshot provider : providerSnapshots) {
+//			LOGGER.info(String.format("Provider that triggered the rule is %s", provider.getName()));
+//			//We filter out all the chirpstack-xxxx which are not of type Teros21
+//			if(provider.getService("sensor") == null) return;
+//			if(provider.getService("sensor").getResource("potentialWater1") == null) return;
+//
+//			String providerId = provider.getName();
+//			GeoJsonObject location = (GeoJsonObject) provider.getService("admin").getResource("location").getValue().getValue();
+//			if(location != null) {
+//				resourceUpdater.updateResource(provider.getName().concat("-derived"), "admin", "location", location, Instant.now());
+//				LOGGER.info(String.format("Updated location for provider %s", providerId));
+//			}			
+//		}
 
 
 
 	}
 
 	private ProviderSnapshot findRuleTriggerProvider(List<ProviderSnapshot> providerSnapshots) {
+		for(ProviderSnapshot provider : providerSnapshots) {
+			System.out.println(String.format("Name %s - Timestamp %s", provider.getName(), provider.getSnapshotTime()));
+		}
 		return providerSnapshots.stream().max(Comparator.comparing(ProviderSnapshot::getSnapshotTime)).orElseGet(null);
 	}
 
